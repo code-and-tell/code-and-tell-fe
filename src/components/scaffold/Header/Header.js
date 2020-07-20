4e01c7c723733177a006a98a3b4b4307bf82033e
Malcolm Kiano <malcolmkiano@gmail.com>
2020-07-20 13:58:21 -0400

    feat: update header, theme and fonts

--- src/components/scaffold/Header/Header.js
+++ src/components/scaffold/Header/Header.js
@@ -13,31 +13,29 @@ import { AppContext } from 'contexts/AppContext/AppContext';
 
 // Files / Assets
 import './Header.scss';
-import Logo from 'assets/images/logo-temp.png';
 
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
-  
+
   // Initialize our state
   const { dispatch } = useContext(AppContext);
 
-  const headerText = <h1>Code<span className="amp">&&</span>Tell</h1>;
+  const headerText = <h1>CODE<span className="amp">&&</span>TELL</h1>;
 
   return (
     <div className="Header">
 
-      <Link 
+      <Link
         to="/"
         className="Logo"
       >
-        <img src={ Logo } alt="logo" />
-        { headerText }
+        {headerText}
       </Link>
 
       {/* Will convert to component due to deeper business logic later */}
-      <div 
+      <div
         className="MenuIcon"
         onClick={(e) => dispatch({
           type: "toggle-menu"
